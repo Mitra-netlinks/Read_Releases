84aefbdac95acbd1cea997c31cfd15a06851319f
Mitra-netlinks <39728277+Mitra-netlinks@users.noreply.github.com>
2018-12-18 09:51:51 +0430

    Update read_release.py

--- read_release.py
+++ read_release.py
@@ -4,8 +4,11 @@ import xlsxwriter
 from github import BadCredentialsException
 
 
-username = raw_input("Github username: ")
+username = input("Github username: ")
 password = getpass.getpass(prompt='Github password: ')
+repo_name = input("Repository name: ")
+
+url = str(username+'/'+repo_name)
 
 if username !='' and password !='':
 
@@ -15,37 +18,35 @@ if username !='' and password !='':
 	for i in range(1, 4):
 		worksheet.set_column(i, i, 30)
 
+	try:
 
-	g = Github(username, password)
+		g = Github(username, password)
 
-	repo = g.get_repo('Mitra-netlinks/test_repo')
-	worksheet.write(0, 0, 'Client Version')
-	worksheet.write(0, 1, 'Version reference for Internal purposes')
-	worksheet.write(0, 2, 'Proposed Change (Excepted Functional Behavior)')
-	# worksheet.write(0, 3, 'BUG FIX?')
-	
-	row = 1
-	k = 0
-	for release in repo.get_releases():
+		repo = g.get_repo(url)
+		worksheet.write(0, 0, 'Client Version')
+		worksheet.write(0, 1, 'Version reference for Internal purposes')
+		worksheet.write(0, 2, 'Proposed Change (Excepted Functional Behavior)')
 		
-		worksheet.write(row, 0, release.tag_name)
-		worksheet.write(row, 1, release.title)
-		worksheet.write(row, 2, release.body.splitlines)
-		# worksheet.write(row, 3, release.body)
+		row = 1
+		for release in repo.get_releases():
+			
+			worksheet.write(row, 0, release.tag_name)
+			worksheet.write(row, 1, release.title)
+			line = (release.body.splitlines)
+			line_to_read = line()
+			for l in line_to_read:
+				worksheet.write(row,2,l)
+				row += 1
 
-		line = (body.splitlines)
-		line_to_read = line(0)
-		for l in line_to_read:
-			worksheet.write_row(row,k, [tag_name, title, l])
+		 
+		print ('Excel file created successfully.')
+	except BadCredentialsException:
+		print ('Bad credentials. Try again!')
+	finally:
 
-	 
-		row += 1
-
-	print 'Excel file created successfully.'
-
-	workbook.close()
+		workbook.close()
 
 else:
-	print 'Github credentials are required!'
+	print ('Github credentials are required!')
 
 		
